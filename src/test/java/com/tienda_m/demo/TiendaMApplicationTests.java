3c3e7d26eeafdbb7e73bc036d10bedffa0701362
= <=>
2024-05-22 20:50:50 -0600

    Primer commit

---
+++ src/test/java/com/tienda_m/demo/TiendaMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda_m.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
